a0b42c72b23b326d652948588ed13492bff28b02
Marvin Ramos <mr487223@gmail.com>
2024-01-24 13:18:06 -0600

    add index file

---
+++ index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import connectDB from './shared/index';
+import "dotenv/config";
+
+const app = express();
+
+app.use(express.json());
+app.disable("x-powered-by");
+
+
+
+connectDB();
+app.listen(process.env.PORT || 3000, ()=>{console.log(`♿ Server is running on: http://localhost:${process.env.PORT}`)});
