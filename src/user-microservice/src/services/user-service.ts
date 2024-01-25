03dcb44f946380527ace943c4c9237f519c33864
Marvin Ramos <mr487223@gmail.com>
2024-01-25 00:41:44 -0600

    add feature for user register

--- src/user-microservice/src/services/user-service.ts
+++ src/user-microservice/src/services/user-service.ts
@@ -1,15 +1,15 @@
 import userModel  from "../models/user-model";
 import { Request, Response } from "express";
-import bcrypt from "bcryptjs";
+// import bcrypt from "bcryptjs";
 
 const userService = {
     async create(req: Request){
-        const { password } = req.body;
+        // const { password } = req.body;
 
-        const salt:string = bcrypt.genSaltSync(10);
-		const hash:string = bcrypt.hashSync(password, salt);
+        // const salt:string = bcrypt.genSaltSync(10);
+		// const hash:string = bcrypt.hashSync(password, salt);
 
-        req.body.password = hash;
+        // req.body.password = hash;
 
         const user = await userModel.create(req.body)
 
