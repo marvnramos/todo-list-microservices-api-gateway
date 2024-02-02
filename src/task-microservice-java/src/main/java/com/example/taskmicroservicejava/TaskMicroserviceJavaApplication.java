9657d6893b6744d83e271244e89cb435c1ac5b60
Marvin Ramos <mr487223@gmail.com>
2024-02-02 13:18:59 -0600

    add task java server

---
+++ src/task-microservice-java/src/main/java/com/example/taskmicroservicejava/TaskMicroserviceJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskmicroservicejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMicroserviceJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskMicroserviceJavaApplication.class, args);
+	}
+
+}
