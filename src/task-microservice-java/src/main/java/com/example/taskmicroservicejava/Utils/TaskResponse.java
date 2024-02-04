436ba2730bf17306ce3020549d3ae9a782d17eb2
Marvin Ramos <mr487223@gmail.com>
2024-02-04 01:35:18 -0600

    excluding null properties from the json response

--- src/task-microservice-java/src/main/java/com/example/taskmicroservicejava/Utils/TaskResponse.java
+++ src/task-microservice-java/src/main/java/com/example/taskmicroservicejava/Utils/TaskResponse.java
@@ -1,6 +1,7 @@
 package com.example.taskmicroservicejava.Utils;
 
 import com.example.taskmicroservicejava.Models.TaskModel;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,6 +18,13 @@ import java.util.List;
 public class TaskResponse {
     @JsonProperty("message")
     private String message;
+
     @JsonProperty("tasks")
-    private List<TaskModel> task;
+    @JsonInclude(JsonInclude.Include.NON_NULL) // debe excluir la propiedad tasks si su valor es null.
+    private List<TaskModel> tasks;
+
+    @JsonProperty("task")
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private TaskModel task;
+
 }
