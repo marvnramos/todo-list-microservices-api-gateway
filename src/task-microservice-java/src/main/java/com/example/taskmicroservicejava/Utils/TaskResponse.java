cdd3180fc884c0fc534c19e7a3a08cbadfa40980
Marvin Ramos <mr487223@gmail.com>
2024-02-04 00:58:45 -0600

    refactoring task response

--- src/task-microservice-java/src/main/java/com/example/taskmicroservicejava/Utils/TaskResponse.java
+++ src/task-microservice-java/src/main/java/com/example/taskmicroservicejava/Utils/TaskResponse.java
@@ -1,32 +1,22 @@
 package com.example.taskmicroservicejava.Utils;
 
 import com.example.taskmicroservicejava.Models.TaskModel;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.aspectj.weaver.ast.Var;
-import org.springframework.scheduling.config.Task;
+
+
+import java.util.List;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class TaskResponse {
+    @JsonProperty("message")
     private String message;
-    private TaskModel task;
-
-    public String getMessage() {
-        return message;
-    }
-    public TaskModel getTask() {
-        return task;
-    }
-    public void setMessage(String message) {
-        this.message = message;
-    }
-    public void setTask(TaskModel task) {
-        this.task = task;
-    }
-
+    @JsonProperty("tasks")
+    private List<TaskModel> task;
 }
