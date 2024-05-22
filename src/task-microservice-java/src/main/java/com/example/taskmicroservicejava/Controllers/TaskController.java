d1171f1d1d933d9528c0160d9b4608b326fc785e
Marvin Ramos <mr487223@gmail.com>
2024-05-22 14:51:03 -0600

    refactor: add cors to api endpoints

--- src/task-microservice-java/src/main/java/com/example/taskmicroservicejava/Controllers/TaskController.java
+++ src/task-microservice-java/src/main/java/com/example/taskmicroservicejava/Controllers/TaskController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping(path = "/tasks")
 public class TaskController {
