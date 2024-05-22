421fea09113c6db588759033be332c82b7187cb1
Marvin Ramos <mr487223@gmail.com>
2024-05-22 11:52:51 -0600

    refactor: add getByUserId method to map user tasks

--- src/task-microservice-java/src/main/java/com/example/taskmicroservicejava/Repositories/TaskRepository.java
+++ src/task-microservice-java/src/main/java/com/example/taskmicroservicejava/Repositories/TaskRepository.java
@@ -4,11 +4,13 @@ import com.example.taskmicroservicejava.Models.TaskModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.scheduling.config.Task;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface TaskRepository extends JpaRepository<TaskModel, Long> {
     Optional<TaskModel> findById(UUID id);
     TaskModel getById(UUID id);
+    List<TaskModel> getByUserId(String userId);
 
 }
