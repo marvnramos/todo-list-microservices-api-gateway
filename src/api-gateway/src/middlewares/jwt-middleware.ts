a9c1891dd80774483fba0480db48019e2d227e09
Marvin Ramos <mr487223@gmail.com>
2024-05-24 10:50:36 -0600

    refactor: Update jwt-middleware to use user-service endpoint for user retrieval

--- src/api-gateway/src/middlewares/jwt-middleware.ts
+++ src/api-gateway/src/middlewares/jwt-middleware.ts
@@ -1,10 +1,11 @@
 import jwt from "jsonwebtoken";
-import UserModel  from "../models/user-model";
+import axios from "axios";
 import { Request, Response, NextFunction } from "express";
 import "dotenv/config";
 
 const authValidation = async (req: Request, res: Response, next: NextFunction) => {
 	const authorizationHeader = req.headers.authorization;
+	const user = process.env.USER_SERVICE_URL || "http://localhost:3000";
 
 	if (!authorizationHeader) {
 		return res.status(401).json({ message: "No se ha enviado el token" });
@@ -19,13 +20,12 @@ const authValidation = async (req: Request, res: Response, next: NextFunction) =
 	try {
 		const decoded:any = jwt.verify(token, process.env.JWT_SECRET!);
 
-		const user = await UserModel.findById(decoded.sub);
-		if (!user) {
+		const response = await axios.get(`${user}/users/get/${decoded.sub}`);
+		if (!response.data) {
 			return res.status(401).json({ message: `Usuario inválido` });
 		}
 
 		req.body.userId = decoded.sub;
-
 		next();
 	} catch (error) {
 		return res.status(500).json({ message: "El token no es válido" });
