fff4564b8c1bf3e3e5c639ed71554431d66d97f0
Marvin Ramos <mr487223@gmail.com>
2024-05-24 10:53:23 -0600

    refactor: Remove unused MongoDB configuration file

--- src/api-gateway/src/index.ts
+++ src/api-gateway/src/index.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import userRoutes from './routes/user-route';
 import taskRoutes from './routes/task-route';
-import connectDB from './config/mongodb-config';
 import "dotenv/config";
 
 
@@ -13,7 +12,6 @@ app.use(express.json());
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/tasks', taskRoutes);
 
-connectDB();
 app.listen(PORT, () => {
   console.log(`☝️ 🤓 API Gateway is running on: http://localhost:${PORT}`)
 });
