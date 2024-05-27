332246f7fcf720008ef298a506707881ef687bb8
Marvin Ramos <mr487223@gmail.com>
2024-05-27 12:26:39 -0600

    refactor: Add getByUserId and getById methods to TaskController

--- src/api-gateway/src/routes/task-route.ts
+++ src/api-gateway/src/routes/task-route.ts
@@ -1,14 +1,16 @@
 import { Router } from "express";
 import TaskController from "../controllers/task-controller";
 import authValidation from "../middlewares/jwt-middleware";
+import { Response, Request } from "express";
 
 const router: Router = Router();
 
+router.get("/user", authValidation, TaskController.getByUserId);
+router.get("/:id", authValidation, TaskController.getById);
 router.get("/", authValidation, TaskController.getAll);
-router.get("/:id",authValidation , TaskController.getById);
+
 router.post("/create", authValidation, TaskController.create);
 router.patch("/edit/:id", authValidation, TaskController.update);
 router.delete("/delete/:id", authValidation, TaskController.delete);
-
 
 export default router;
