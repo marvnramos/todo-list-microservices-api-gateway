1ca33b6bc2bbba8c6cdd2a7782775a583e71fb2a
Marvin Ramos <mr487223@gmail.com>
2024-05-27 08:39:18 -0600

    refactor: Update jwtService implementation in jwt.ts

--- src/user-microservice/src/utils/jwt.ts
+++ src/user-microservice/src/utils/jwt.ts
@@ -1,25 +1,44 @@
 import jwt from "jsonwebtoken";
 import Body from "../interfaces/AuthPayload-interface";
 
-const generateToken = (body: Body): Promise<String> => {
+export default class jwtService {
+	static generateToken(body: Body): Promise<String> {
+		return new Promise((resolve, reject) => {
+			const { id: sub, loged} = body;
+	
+			const payload = {
+				sub,
+				loged
+			};
+	
+			jwt.sign(
+				payload,
+				process.env.JWT_SECRET! || "secret",
+				{ expiresIn: process.env.JWT_EXPIRES_IN!  || "1h"},
+				(err, token: any) => {
+					if (err) reject(err);
+					resolve(token);
+				}
+			);
+		});
+	}
+
+	static validate(token: string): Promise<any> {
+		return new Promise((resolve, reject) => {
+			jwt.verify(token, process.env.JWT_SECRET! || "secret", (err, decoded) => {
+					if (err) reject(err);
+					resolve(decoded);
+				});
+		})
+	};
+}
+
+
+export const validate = (token: string): Promise<any> => {
 	return new Promise((resolve, reject) => {
-		const { id: sub, loged} = body;
-
-		const payload = {
-			sub,
-			loged
-		};
-
-		jwt.sign(
-			payload,
-			process.env.JWT_SECRET!,
-			{ expiresIn: process.env.JWT_EXPIRES_IN! },
-			(err, token: any) => {
+		jwt.verify(token, process.env.JWT_SECRET! || "secret", (err, decoded) => {
 				if (err) reject(err);
-				resolve(token);
-			}
-		);
-	});
+				resolve(decoded);
+			});
+	})
 };
-
-export default generateToken;
