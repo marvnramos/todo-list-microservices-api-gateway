d0a942ca1f98486891a2c7234f87afe336196775
Marvin Ramos <mr487223@gmail.com>
2024-05-27 08:36:52 -0600

    feat: Add authentication route for user

--- src/user-microservice/src/routes/user-route.ts
+++ src/user-microservice/src/routes/user-route.ts
@@ -5,6 +5,7 @@ const router: Router = Router();
 
 // Registro de usuario
 router.post("/", userController.create);
+router.get("/auth", userController.auth)
 router.post("/login", userController.logIn);
 router.patch("/edit/:id", userController.update);
 router.delete("/delete/:id", userController.delete);
